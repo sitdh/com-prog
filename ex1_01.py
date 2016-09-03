56e7570f1e03372dd2231b2db58e2393bd41dc08
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-09-04 00:02:17 +0700

    Remove mulply by -1

--- ex1_01.py
+++ ex1_01.py
@@ -2,11 +2,11 @@ import math
 
 x = float(input())
 
-prop_2 = -1 * (x**2) / math.factorial(2)
+prop_2 = -(x**2) / math.factorial(2)
 
 prop_3 = (x**4) / math.factorial(4)
 
-prop_4 = -1 * (x**6) / math.factorial(6)
+prop_4 = -(x**6) / math.factorial(6)
 
 cos_x = float(1 + prop_2 + prop_3 + prop_4)
 
